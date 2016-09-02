b7979bd700b1bf2441a0d89939425c76b586bfbd
Prashanth <janagama.prashanth@gmail.com>
2016-09-01 22:37:39 -0400

    render

--- App.js
+++ App.js
@@ -19,6 +19,22 @@ class App extends React.Component {
 */
 
 /*Intro Alternate Syntax. This will not have state.*/
+/*
 const App = () => <h1>Hello Eggheads</h1>
+*/
+
+class App extends React.Component {
+	render(){
+		return(
+              <div>
+                <h1>Hello World</h1>
+                <b>Bold</b>
+              </div>
+				
+			)    
+
+		//<h1>Hello World</h1> //<b>Bold</b> Wont work with Wrap in single node
+	}
+}
 
 export default App
