7097964055c8fefee7424f21951b7db4ccfeeba0
Prashanth <janagama.prashanth@gmail.com>
2016-09-01 22:34:47 -0400

    intro-lesson

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+/* Lesson 1
 class App extends React.Component {
 	render(){
 		return <div> Hi </div>
 	}
 }
+*/
+
+
+/* Intro  Regular Class Component can have state*/
+/*
+class App extends React.Component {
+	render(){
+		return <h1>Hello World</h1>
+	}
+}
+*/
+
+/*Intro Alternate Syntax. This will not have state.*/
+const App = () => <h1>Hello Eggheads</h1>
 
 export default App
